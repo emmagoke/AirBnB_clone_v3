cee346ae55eaf3e74e30702a3a55d375dcd98aa9
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-05-03 10:50:09 -0700

    Added task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,9 +7,11 @@ from flask import make_response
 from flask import jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
